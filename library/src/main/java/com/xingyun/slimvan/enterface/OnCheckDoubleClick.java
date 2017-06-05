7de2f4d1f6d083ab0ba7774cecc4016fb53f7632
slimvan <slimvan@163.com>
2017-06-05 14:38:23 +0800

    to library

---
+++ library/src/main/java/com/xingyun/slimvan/enterface/OnCheckDoubleClick.java
@@ -0,0 +1,16 @@
+package com.xingyun.slimvan.enterface;
+
+import android.view.View;
+
+/**
+ * 自定义点击监听
+ * 防止快速双击执行两次点击事件  回调interface
+ * Created by xingyun on 2017/3/9.
+ */
+
+/**
+ * Created by slimvan on 2016/12/29.
+ */
+public interface OnCheckDoubleClick {
+    void onCheckDoubleClick(View view);
+}
---
+++ library/src/main/java/com/xingyun/slimvan/listener/CheckDoubleClickListener.java
@@ -0,0 +1,36 @@
+package com.xingyun.slimvan.listener;
+
+import android.view.View;
+
+import com.xingyun.slimvan.enterface.OnCheckDoubleClick;
+
+import java.util.Calendar;
+
+/**
+ * 自定义点击监听
+ * 防止快速双击执行两次点击事件
+ * Created by xingyun on 2017/3/9.
+ */
+
+/**
+ *
+ * Created by slimvan on 2016/12/29.
+ */
+public class CheckDoubleClickListener implements View.OnClickListener {
+    public static final int MIN_CLICK_DELAY_TIME = 1000;
+    private long lastClickTime = 0;
+    private OnCheckDoubleClick checkDoubleClick;
+
+    public CheckDoubleClickListener(OnCheckDoubleClick checkDoubleClick){
+        this.checkDoubleClick = checkDoubleClick;
+    }
+
+    @Override
+    public void onClick(View v) {
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        if (currentTime - lastClickTime > MIN_CLICK_DELAY_TIME) {
+            lastClickTime = currentTime;
+            checkDoubleClick.onCheckDoubleClick(v);
+        }
+    }
+}
